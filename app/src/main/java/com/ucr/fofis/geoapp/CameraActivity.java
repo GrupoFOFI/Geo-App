54600588d673d4b5e85ea60dc212fb1c9a5c1117
William Soto Martinez <b36886@ecci.ucr.ac.cr>
2017-05-11 10:48:16 -0600

    Runtime Camera Permissions

--- app/src/main/java/com/ucr/fofis/geoapp/CameraActivity.java
+++ app/src/main/java/com/ucr/fofis/geoapp/CameraActivity.java
@@ -1,13 +1,18 @@
 package com.ucr.fofis.geoapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+
+import static android.content.pm.PackageManager.PERMISSION_DENIED;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -19,10 +24,26 @@ public class CameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        try{
-            mCamera = Camera.open();
-        }catch (Exception e){
-            Log.d("ERROR", "Failed to get camera "+e.getMessage());
+
+        // Assume thisActivity is the current activity
+        int cameraPermissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+
+        if(cameraPermissionCheck != PackageManager.PERMISSION_GRANTED){
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)){
+
+                // Explanation
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        1);
+            }
         }
 
         if(mCamera != null){
@@ -41,5 +62,31 @@ public class CameraActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+                    try{
+                        mCamera = Camera.open();
+                    }catch (Exception e){
+                        Log.d("ERROR", "Failed to get camera "+e.getMessage());
+                    }
+
+
+                } else {
+
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 }
