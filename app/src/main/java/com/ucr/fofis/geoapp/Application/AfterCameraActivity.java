19a519766f8398328176fb2bc230f03f46177521
Oscar Corella Quiros <b32080@ecci.ucr.ac.cr>
2017-06-05 10:23:16 -0600

    after camera activity and layout added

---
+++ app/src/main/java/com/ucr/fofis/geoapp/Application/AfterCameraActivity.java
@@ -0,0 +1,18 @@
+package com.ucr.fofis.geoapp.Application;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ucr.fofis.geoapp.R;
+
+/**
+ * Created by b32080 on 05/06/2017.
+ */
+
+public class AfterCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.after_camera_activity);
+    }
+}
