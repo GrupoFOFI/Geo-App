150620733b46dfcaf2f4cc3a35df96b171cd7d80
Sotwi <admlab105@ecci.ucr.ac.cr>
2017-06-12 14:37:26 -0600

    Videos work now

--- app/src/main/java/com/ucr/fofis/geoapp/VideoDialog.java
+++ app/src/main/java/com/ucr/fofis/geoapp/VideoDialog.java
@@ -2,11 +2,10 @@ package com.ucr.fofis.geoapp;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.View;
-import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.ucr.fofis.dataaccess.entity.Punto;
@@ -17,35 +16,31 @@ import com.ucr.fofis.dataaccess.entity.Punto;
 
 public class VideoDialog extends Dialog {
 
+    Context cntx = null;
+
 
     public VideoDialog(@NonNull Context context) {
         super(context);
+        cntx = context;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setTitle("Video");
-        setContentView(R.layout.video_dialog);
 
-        final VideoView vid = (VideoView) findViewById(R.id.video_view);
-        String uriPath = "android.resource://com.ucr.fofis.dataaccess/"+R.raw.v29_6;
-        Uri uri = Uri.parse(uriPath);
-        vid.setVideoURI(uri);
-        vid.requestFocus();
-        vid.start();
         Punto p =  (Punto) getOwnerActivity().getIntent().getSerializableExtra("punto");
-        p.getVideoId();
-        Button dialogButton = (Button) findViewById(R.id.video_button);
-        dialogButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-                public void onClick(View v) {
-                    dismiss();
-                }
-            }
 
-        );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_dialog);
+        //setTitle(p.getNombre());
+        setTitle("Video");
 
+        VideoView vidVw = (VideoView) findViewById(R.id.video_view);
+        //vidVw.setVideoPath("android.resource://com.ucr.fofis.geoapp/"+p.getVideoId());
+        vidVw.setVideoPath("android.resource://com.ucr.fofis.geoapp/"+R.raw.video06_3);
+        vidVw.requestFocus();
+        vidVw.setZOrderOnTop(true);
+
+        vidVw.start();
     }
 
 }
