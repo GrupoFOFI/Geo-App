076f3997705519432290f679e01c30943415c555
Ricardo Apú <ricardoapuch@gmail.com>
2017-06-12 11:05:42 -0600

    :(

--- app/src/main/java/com/ucr/fofis/geoapp/VideoDialog.java
+++ app/src/main/java/com/ucr/fofis/geoapp/VideoDialog.java
@@ -1,16 +1,15 @@
 package com.ucr.fofis.geoapp;
 
-        import android.app.*;
-        import android.app.Dialog;
-        import android.content.Context;
-        import android.net.Uri;
-        import android.os.Bundle;
-        import android.support.annotation.NonNull;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.Button;
-        import android.widget.VideoView;
+import android.app.Dialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+import com.ucr.fofis.dataaccess.entity.Punto;
 
 /**
  * Created by william on 08/06/17.
@@ -30,12 +29,13 @@ public class VideoDialog extends Dialog {
         setContentView(R.layout.video_dialog);
 
         final VideoView vid = (VideoView) findViewById(R.id.video_view);
-        String uriPath = "andorid.resource://com.ucr.fofis.dataaccess/"+R.raw.video6_3_small;
+        String uriPath = "android.resource://com.ucr.fofis.dataaccess/"+R.raw.v29_6;
         Uri uri = Uri.parse(uriPath);
         vid.setVideoURI(uri);
         vid.requestFocus();
         vid.start();
-
+        Punto p =  (Punto) getOwnerActivity().getIntent().getSerializableExtra("punto");
+        p.getVideoId();
         Button dialogButton = (Button) findViewById(R.id.video_button);
         dialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- scripts/resize_video.sh
+++ scripts/resize_video.sh
@@ -2,6 +2,6 @@
 for video in *.mp4; do
 	newname=$(echo $video | sed "s/\.mp4/_small\.mp4/")
 	echo $newname
-	ffmpeg -i $video -s 640x480 -b:v 3072k -vcodec mpeg1video -acodec copy $newname
+	ffmpeg -i $video -s 640x480 -b:v 3072k -vcodec mp4 -acodec copy $newname
 	rm $video
 done
